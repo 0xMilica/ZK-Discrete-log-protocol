3eb5e19074955fc976803f9aaf1b8eca4258ac94
0xMilica <0xmilica@gmail.com>
2024-03-11 02:19:18 +0100

    project init

---
+++ target/debug/ZK-Discrete-log-protocol-attack.d
@@ -0,0 +1,1 @@
+/Users/milicavulic/ZK-Discrete-log-protocol-attack/target/debug/ZK-Discrete-log-protocol-attack: /Users/milicavulic/ZK-Discrete-log-protocol-attack/src/main.rs
---
+++ target/debug/deps/ZK_Discrete_log_protocol_attack-90478ff66e704cd2.d
@@ -0,0 +1,5 @@
+/Users/milicavulic/ZK-Discrete-log-protocol-attack/target/debug/deps/ZK_Discrete_log_protocol_attack-90478ff66e704cd2: src/main.rs
+
+/Users/milicavulic/ZK-Discrete-log-protocol-attack/target/debug/deps/ZK_Discrete_log_protocol_attack-90478ff66e704cd2.d: src/main.rs
+
+src/main.rs:
